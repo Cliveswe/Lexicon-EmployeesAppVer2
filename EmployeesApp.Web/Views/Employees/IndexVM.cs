97cc1dded7008e1e692b1daf57becd4b12c4924b
albertito1985 <anulo@live.se>
2025-05-20 11:48:44 +0200

    indexVM done

---
+++ EmployeesApp.Web/Views/Employees/IndexVM.cs
@@ -0,0 +1,20 @@
+﻿using EmployeesApp.Web.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesApp.Web.Views.Employees
+{
+    public class IndexVM
+    {
+        public required int Id { get; set; }
+
+        [Required(ErrorMessage = "You must specify a name")]
+        [SuspiciousPerson("Pontus Wittenmark", ErrorMessage = "Suspicious person detected!")]
+        public required string Name { get; set; }
+
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "Invalid e-mail address")]
+        [Required(ErrorMessage = "You must specify an e-mail address")]
+        public required string Email { get; set; }
+        public required bool ShowAsHighlighted { get; set; }
+    }
+}
